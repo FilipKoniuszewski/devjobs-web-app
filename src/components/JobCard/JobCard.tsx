71230047bc16d4e3d9947c78a9c959f51790f6d1
Filip Koniuszewski <filip7446@gmail.com>
2022-10-02 03:15:17 +0200

    fix

--- src/components/JobCard/JobCard.tsx
+++ src/components/JobCard/JobCard.tsx
@@ -21,23 +21,21 @@ export const JobCard: FunctionComponent<JobCardProps> = ({ jobData }) => {
     location,
   } = jobData;
   return (
-    <article>
-      <Link to={`${id}`} className='card'>
-        <div
-          className='card__logo-wrapper'
-          style={{ background: logoBackground }}
-        >
-          <img src={logo} alt={`${company}-logo`} />
-        </div>
-        <div className='job-secondary-info'>
-          <div className='card__posted-at'>{postedAt}</div>
-          <BsCircleFill size={5} />
-          <div className='card__contract'>{contract}</div>
-        </div>
-        <div className='card__job-name'>{position}</div>
-        <div className='card__company-name'>{company}</div>
-        <div className='card__location'>{location}</div>
-      </Link>
-    </article>
+    <Link to={`${id}`} className='card'>
+      <div
+        className='card__logo-wrapper'
+        style={{ background: logoBackground }}
+      >
+        <img src={logo} alt={`${company}-logo`} />
+      </div>
+      <div className='job-secondary-info'>
+        <div className='card__posted-at'>{postedAt}</div>
+        <BsCircleFill size={5} />
+        <div className='card__contract'>{contract}</div>
+      </div>
+      <div className='card__job-name'>{position}</div>
+      <div className='card__company-name'>{company}</div>
+      <div className='card__location'>{location}</div>
+    </Link>
   );
 };
